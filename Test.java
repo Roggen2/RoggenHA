4deaeea598126addd2b06dbd38b07d801c88aa50
Nikita Kister <n.kister@tu-bs.de>
2015-05-25 15:38:25 +0200

    Test um BufferedReader erweitert.

--- Test.java
+++ Test.java
@@ -5,10 +5,12 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Test
-{
-   public static void main(String[] args)  {
-      Inventory<Item> i = new Inventory<Item>();
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class Test {
+   public static void main(String[] args) throws IOException  {
+      /*Inventory<Item> i = new Inventory<Item>();
       i.insert(new Item());
       i.insert(new Item());
       i.insert(new Item());
@@ -21,4 +23,14 @@ public class Test
       i.insert(new Item("ababjb"));
       i.insert(new Item("zzz"));
       System.out.println(i);
-}}
+      */
+      BufferedReader b = new BufferedReader(new FileReader("item.csv"));
+      System.out.println(b.readLine());
+      System.out.println(b.readLine());
+      System.out.println(b.readLine());
+      System.out.println(b.readLine());
+      System.out.println(b.readLine());
+	   //BufferedReader br = Files.newBufferedReader(Paths.get("item.csv"));
+ 
+   }
+}
