4574635a7ee358567042e3ca315f11183a0bc80b
Nikita Kister <n.kister@tu-bs.de>
2015-05-28 10:17:57 +0200

    Item implementiert Comparable<T>

--- Item.java
+++ Item.java
@@ -8,23 +8,23 @@
 public class Item implements Comparable<Item> {
     
     /**
-     * Name des Items
-     */
+    * Name des Items
+    */
     private String name;
-
-    /**
-      * Gewicht des Items
-      */
-    private int weight;
     
     /**
-     * Wert des Items
-     */
-    private int value;
+    * Gewicht des Items
+    */
+    private double weight;
     
     /**
-     * erzeugt zufaelliges Item-Objekt
-     */
+    * Wert des Items
+    */
+    private double value;
+    
+    /**
+    * erzeugt zufaelliges Item-Objekt
+    */
     public Item() {
         String[] iList = {"Juwel", "Eisen", "Erde", "Holz", "Schwert", "Schild", "Helm"};
         this.name = iList[(int) (Math.random() * iList.length)];
@@ -33,75 +33,93 @@ public class Item implements Comparable<Item> {
     }
     
     /**
-     * erzeugt zufaelliges Item-Objekt mit dem uebergebenen Namen
-     * @param name ist der Name
-     */
+    * erzeugt zufaelliges Item-Objekt mit dem uebergebenen Namen
+    * @param name ist der Name
+    */
     public Item(String name) {
         this.name = name;
         this.weight = (int) (30 * Math.random() + 1);
         this.value = (int) (50 * Math.random() + 1);
     }
+    /**
+    * gibt den Namen zurueck
+    * @return name
+    */
+    public String getName(){
+        return this.name;
+    }
+    /**
+    * gibt das Gewicht zurueck
+    * @return name
+    */
+    public double getWeight(){
+        return this.weight;
+    }
+    /**
+    * gibt den Wert zurueck
+    * @return name
+    */
+    public double getValue(){
+        return this.value;
+    }
     
     
     
     /**
-     * erzeugt ein neues Item-Objekt mit den uebergebenen Parametern
-     * @param name ist der Name
-     * @param weight ist das Gewicht
-     * @param value ist der Wert
-     */
-    public Item(String name, int weight, int value) {
+    * erzeugt ein neues Item-Objekt mit den uebergebenen Parametern
+    * @param name ist der Name
+    * @param weight ist das Gewicht
+    * @param value ist der Wert
+    */
+    public Item(String name, double weight, double value) {
         this.name = name;
         this.weight = weight;
         this.value = value;
     }
     
     /**
-     * Ueberladung der equals-Methode
-     * @param o ist das zu vergleichende Object
-     * @return ob das uebergebene Object diesem gleicht 
-     */
+    * Ueberladung der equals-Methode
+    * @param o ist das zu vergleichende Object
+    * @return ob das uebergebene Object diesem gleicht 
+    */
     public boolean equals(Object o) {
         Item a = (Item) o;
         return ((this.name.equals(a.name)) && (this.weight == a.weight) && (this.value == a.value));
     }
     
     /**
-     * vergleicht dieses Objekt mit dem uebergebenen
-     * @param o ist das zu vergleichende Object
-     * @return -1 wenn kleiner, 1 wenn groesser, 0 wenn gleich
-     */
+    * vergleicht dieses Objekt mit dem uebergebenen
+    * @param o ist das zu vergleichende Object
+    * @return -1 wenn kleiner, 1 wenn groesser, 0 wenn gleich
+    */
     public int compareTo(Item o) {
-       /* if (!(o instanceof Item)) {
-            throw new IllegalArgumentException("incompatible types");
-        }*///
-        Item a = (Item) o;
-        if (this.name.equals(a.name)) {
-            if (this.value == a.value) {
-                if (this.weight == a.weight) {
+        
+        if (this.name.equals(o.name)) {
+            if (this.value == o.value) {
+                if (this.weight == o.weight) {
                     return 0;
                 } else {
-                    return Integer.signum(a.weight - this.weight);
+                    return ((o.weight - this.weight) < 0.0) ? -1 : 1;
                 }
                 
             } else {
-                return Integer.signum(a.value - this.value);
+                return ((o.value - this.value) < 0.0) ? -1 : 1;
             }
             
             
             
         }
-        return Integer.signum(this.name.compareTo(a.name));
+        return Integer.signum(this.name.compareTo(o.name));
     }
     
     /**
-     * erzeugt einen String mit den Informationen ueber dieses Item
-     * @return den String
-     */
+    * erzeugt einen String mit den Informationen ueber dieses Item
+    * @return den String
+    */
     public String toString() {
         return "Name: " + this.name + "  Gewicht: " + this.weight + "  Wert: " + this.value;
-    
-    
+        
+        
     } 
     
 }
